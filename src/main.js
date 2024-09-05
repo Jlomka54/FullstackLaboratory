db8fcff6f72d172e49edf3d90fbd2f9ab8951218
Bohdan Haponiuk <jlomka54@gmail.com>
2024-09-05 20:11:14 +0400

    add js file section

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import './js/header';
+import './js/hero';
+import './js/about-me';
+import './js/benefits';
+import './js/projects';
+import './js/faq';
+import './js/covers';
+import './js/reviews';
+import './js/work-together';
