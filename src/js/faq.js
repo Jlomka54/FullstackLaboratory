23e0cf93ab48fb493c429d9fdcfad5bd125246be
Mariia_Ostapets <mariyaostapets@gmail.com>
2024-09-06 20:45:00 +0300

    Change name of class from accordion-container to faq-accordion-container

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,7 +1,7 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
-new Accordion('.accordion-container', {
+new Accordion('.faq-accordion-container', {
   duration: 400,
   showMultiple: true,
 });
