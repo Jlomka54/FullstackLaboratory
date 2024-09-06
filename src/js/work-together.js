c7962f0f843ff3c69acbb76478241c9028a30483
grizeus <zaytsewww@gmail.com>
2024-09-06 15:17:28 +0300

    .

--- src/js/work-together.js
+++ src/js/work-together.js
@@ -1,36 +1,65 @@
-"use strict";
+'use strict';
 
-const formData = { client_email: "", client_comment: "" };
+import * as basicLightbox from 'basiclightbox';
+import 'basiclightbox/dist/basicLightbox.min.css';
+import svgSprite from '../img/icon.svg';
+import "../css/work-together.css";
+
+const instance = basicLightbox.create(
+  `
+    <div class="wt-modal-overlay">
+      <div class="wt-modal">
+        <button type="button" class="wt-modal-close-btn">
+          <svg class="wt-icon">
+            <use href="${svgSprite}#icon-close-mob-menu-bt"></use>
+          </svg>
+        </button>
+        <p class="wt-modal-tittle">Thank you for your interest in cooperation!</p>
+        <p class="wt-modal-message">The manager will contact you shortly to discuss further details and opportunities for cooperation. Please stay in touch.</p>
+      </div>
+    </div>
+    `,
+  {
+    onShow: instance => {
+      instance.element().querySelector('.wt-modal-close-btn').onclick =
+        instance.close;
+    },
+  }
+);
+
+instance.show();
+
+const formData = { client_email: '', client_comment: '' };
 
 if (localStorage.length > 0) {
-  const storedData = localStorage.getItem("wt-form-data");
+  const storedData = localStorage.getItem('wt-form-data');
   if (storedData) {
     const { client_email, client_comment } = JSON.parse(storedData);
-    formData.client_email = client_email ?? "";
-    formData.client_comment = client_comment ?? "";
+    formData.client_email = client_email ?? '';
+    formData.client_comment = client_comment ?? '';
   }
 }
 
-const form = document.querySelector(".wt-form");
+const form = document.querySelector('.wt-form');
 
 form.elements.client_email.value = formData.client_email;
 form.elements.client_comment.value = formData.client_comment;
 
-form.addEventListener("input", (e) => {
+form.addEventListener('input', e => {
   formData[e.target.name] = e.target.value.trim();
-  localStorage.setItem("wt-form-data", JSON.stringify(formData));
+  localStorage.setItem('wt-form-data', JSON.stringify(formData));
 });
 
-form.addEventListener("submit", (e) => {
+form.addEventListener('submit', e => {
   e.preventDefault();
 
-  if (formData.client_email === "" || formData.client_comment === "") {
+  if (formData.client_email === '' || formData.client_comment === '') {
     return;
   }
 
   console.log(formData);
-  formData.client_email = "";
-  formData.client_comment = "";
-  localStorage.removeItem("wt-form-data");
+  formData.client_email = '';
+  formData.client_comment = '';
+  localStorage.removeItem('wt-form-data');
   form.reset();
 });
